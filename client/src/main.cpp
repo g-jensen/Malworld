d82f9b268317093970b359044f7bff0ddc54d73f
g-jensen <greg.micah.jensen@gmail.com>
2022-10-07 17:51:32 -0400

    Added json library

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <fstream>
 
 #include "../headers/Game.hpp"
 
+#include "../../nlohmann-json/single_include/nlohmann/json.hpp"
+using json = nlohmann::json;
+
 int main() {
     
+    std::ifstream f("test.json");
+    json data = json::parse(f);
+    std::cout << data << std::endl;
+
     Game* game = new Game();
 
     game->init();
